402c99435e420ffb91b9fcf55e5e72f6127c9122
ben-tanen <ben.tanen@gmail.com>
2024-03-13 16:04:21 -0600

    tweak to toc.json.py to follow page structure

--- docs/data/toc.json.py
+++ docs/data/toc.json.py
@@ -1,19 +1,33 @@
 
-import os, sys, json
+import os, sys, json, re
+import esprima
 import frontmatter
 
+# parse observable config file for page structure
+config_file = "observablehq.config.ts"
+assert os.path.isfile(config_file)
+config = esprima.parseModule(open(config_file).read())
+
+def parseArrayExpression(ae):
+    return([{p0.key.name: (p0.value.value if p0.value.type == "Literal" else parseArrayExpression(p0.value)) for p0 in p.properties} for p in ae.elements])
+pages_config = parseArrayExpression([c for c in config.body[0].declaration.properties if c.key.name == "pages"][0].value)
+
+# get create flat pages list with depth + frontmatter
 root = "./docs/"
-toc = []
+pages = []
 
-for subdir, dirs, files in os.walk(root):
-    for file in files:
-        file_path = os.path.join(subdir, file)
-        if os.path.isfile(file_path) and (file_path[-3:] == ".md" or file_path[-9:] == ".markdown"):
-            with open(file_path) as file:
-                file_frontmatter = frontmatter.loads(file.read())
-                toc.append({
-                    **{"file": file_path},
-                    **{k: file_frontmatter[k] for k in file_frontmatter.keys()}
-                })
+def parsePage(p, depth = 0):
+    file = p["path"] + (".md" if os.path.isfile(root + p["path"] + ".md") else ".markdown")
+    assert os.path.isfile(root + file)
+    file_frontmatter = frontmatter.loads(open(root + file).read())
+    return { **p, **{ "depth": depth, "file": file }, **{k: file_frontmatter[k] for k in file_frontmatter.keys()}}
 
-json.dump(toc, sys.stdout)
+for p0 in pages_config:
+    if p0.get('pages'):
+        for p1 in p0['pages']:
+            pages.append(parsePage(p1, depth = 1))
+    else:
+        pages.append(parsePage(p0))
+
+# export
+json.dump(pages, sys.stdout)
